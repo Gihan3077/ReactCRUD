cf72a2fe797a626a654fe9d46761e34f94aea931
Gihan3077 <115445456+Gihan3077@users.noreply.github.com>
2023-07-02 23:02:45 +0530

    1st attempt

---
+++ src/components/Employees.js
@@ -0,0 +1,17 @@
+const Employees = [
+    {
+        id:"1",
+        Name:"Gihan Ishara",
+        Age:"22"
+    },{
+        id:"2",
+        Name:"Kumar Sangakkara",
+        Age:"32"
+    },{
+        id:"3",
+        Name:"Sanath Jayasuriya",
+        Age:"35"
+    }
+]
+
+export default Employees;
